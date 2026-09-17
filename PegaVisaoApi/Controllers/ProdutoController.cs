254345bd4774065ec8bf9b00d0f07acb8b3d3439
unknown <giovanestopacordeirodev@gmail.com>
2026-09-17 00:12:22 -0300

    Buscando erro delete

--- PegaVisaoApi/Controllers/ProdutoController.cs
+++ PegaVisaoApi/Controllers/ProdutoController.cs
@@ -212,17 +212,28 @@ namespace PegaVisaoApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeletarProduto(int id)
         {
-            var produto = _context.Produtos
-                .FirstOrDefault(p => p.Id == id);
+            try
+            {
+                var produto = _context.Produtos
+                    .FirstOrDefault(p => p.Id == id);
 
-            if (produto == null)
-                return NotFound();
+                if (produto == null)
+                    return NotFound();
 
-            _context.Produtos.Remove(produto);
+                _context.Produtos.Remove(produto);
+                _context.SaveChanges();
 
-            _context.SaveChanges();
-
-            return NoContent();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    mensagem = "Erro ao excluir produto.",
+                    erro = ex.Message,
+                    inner = ex.InnerException?.Message
+                });
+            }
         }
     }
 }
