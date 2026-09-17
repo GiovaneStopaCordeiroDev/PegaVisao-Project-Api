29dea15756351211155a46a684969a321c30a529
unknown <giovanestopacordeirodev@gmail.com>
2026-09-17 00:10:22 -0300

    Alterando CORS

--- PegaVisaoApi/Program.cs
+++ PegaVisaoApi/Program.cs
@@ -1,15 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using PegaVisaoApi.Data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddCors(options =>
@@ -23,7 +24,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<PegaVisaoContext>(options =>
@@ -31,18 +31,17 @@ builder.Services.AddDbContext<PegaVisaoContext>(options =>
 
 var app = builder.Build();
 
-app.UseCors("Frontend");
-
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
-
 app.UseHttpsRedirection();
+
+app.UseCors("Frontend");
 
 app.UseAuthorization();
 
