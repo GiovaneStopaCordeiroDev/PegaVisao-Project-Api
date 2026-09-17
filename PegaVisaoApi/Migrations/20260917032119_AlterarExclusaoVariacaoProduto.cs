32f775255c72771284214f0e35907455f168e9d8
unknown <giovanestopacordeirodev@gmail.com>
2026-09-17 00:21:40 -0300

    Corrige exclusao em cascata de produtos

---
+++ PegaVisaoApi/Migrations/20260917032119_AlterarExclusaoVariacaoProduto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PegaVisaoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarExclusaoVariacaoProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemPedidos_VariacaoProdutos_VariacaoProdutoId",
+                table: "ItemPedidos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemPedidos_VariacaoProdutos_VariacaoProdutoId",
+                table: "ItemPedidos",
+                column: "VariacaoProdutoId",
+                principalTable: "VariacaoProdutos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemPedidos_VariacaoProdutos_VariacaoProdutoId",
+                table: "ItemPedidos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemPedidos_VariacaoProdutos_VariacaoProdutoId",
+                table: "ItemPedidos",
+                column: "VariacaoProdutoId",
+                principalTable: "VariacaoProdutos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
