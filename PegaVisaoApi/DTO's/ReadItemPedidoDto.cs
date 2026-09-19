bffffaa9f0d5dec777f036cec0360b23f29eb65d
unknown <giovanestopacordeirodev@gmail.com>
2026-09-19 12:56:57 -0300

    Alterando itemPedidos com cor e produto

--- PegaVisaoApi/DTO's/ReadItemPedidoDto.cs
+++ PegaVisaoApi/DTO's/ReadItemPedidoDto.cs
@@ -3,8 +3,19 @@
     public class ReadItemPedidoDto
     {
         public int Id { get; set; }
+
         public int VariacaoProdutoId { get; set; }
+
         public int Quantidade { get; set; }
+
         public decimal PrecoUnitario { get; set; }
+
+        public string NomeProduto { get; set; }
+
+        public string ImagemProduto { get; set; }
+
+        public string Cor { get; set; }
+
+        public string Tamanho { get; set; }
     }
 }
--- PegaVisaoApi/Profiles/PedidoProfile.cs
+++ PegaVisaoApi/Profiles/PedidoProfile.cs
@@ -9,10 +9,38 @@ namespace PegaVisaoApi.Profiles
         public PedidoProfile()
         {
             CreateMap<CreatePedidoDto, Pedido>();
+
             CreateMap<CreateItemPedidoDto, ItemPedido>();
+
             CreateMap<UpdatePedidoDto, Pedido>();
+
             CreateMap<Pedido, ReadPedidoDto>();
-            CreateMap<ItemPedido, ReadItemPedidoDto>();
+
+            CreateMap<ItemPedido, ReadItemPedidoDto>()
+                .ForMember(
+                    destino => destino.NomeProduto,
+                    opt => opt.MapFrom(
+                        origem => origem.VariacaoProduto.Produto.Nome
+                    )
+                )
+                .ForMember(
+                    destino => destino.ImagemProduto,
+                    opt => opt.MapFrom(
+                        origem => origem.VariacaoProduto.Produto.ImagemPrincipal
+                    )
+                )
+                .ForMember(
+                    destino => destino.Cor,
+                    opt => opt.MapFrom(
+                        origem => origem.VariacaoProduto.Cor
+                    )
+                )
+                .ForMember(
+                    destino => destino.Tamanho,
+                    opt => opt.MapFrom(
+                        origem => origem.VariacaoProduto.Tamanho
+                    )
+                );
         }
     }
 }
