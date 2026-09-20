5d570373a063666fe0041b342b92ad746b16f22b
unknown <giovanestopacordeirodev@gmail.com>
2026-09-20 13:29:20 -0300

    Adiciona webhook do Mercado Pago

---
+++ PegaVisaoApi/Controllers/MercadoPagoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PegaVisaoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MercadoPagoController : ControllerBase
+    {
+        [HttpPost("webhook")]
+        [AllowAnonymous]
+        public IActionResult Webhook([FromBody] object notificacao)
+        {
+            Console.WriteLine("Webhook Mercado Pago recebido:");
+            Console.WriteLine(notificacao);
+
+            return Ok();
+        }
+    }
+}
