75d76a251dd3c425e0a1b5b2835410f4d048bf8e
unknown <giovanestopacordeirodev@gmail.com>
2026-09-20 13:31:14 -0300

    Processa notificacao do Mercado Pago

--- PegaVisaoApi/Controllers/MercadoPagoController.cs
+++ PegaVisaoApi/Controllers/MercadoPagoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace PegaVisaoApi.Controllers
 {
@@ -9,10 +10,27 @@ namespace PegaVisaoApi.Controllers
     {
         [HttpPost("webhook")]
         [AllowAnonymous]
-        public IActionResult Webhook([FromBody] object notificacao)
+        public IActionResult Webhook(
+            [FromBody] JsonElement notificacao)
         {
-            Console.WriteLine("Webhook Mercado Pago recebido:");
-            Console.WriteLine(notificacao);
+            Console.WriteLine("=================================");
+            Console.WriteLine("WEBHOOK MERCADO PAGO RECEBIDO");
+            Console.WriteLine(notificacao.ToString());
+            Console.WriteLine("=================================");
+
+            if (notificacao.TryGetProperty(
+                "data",
+                out var data) &&
+                data.TryGetProperty(
+                    "id",
+                    out var id))
+            {
+                var paymentId = id.ToString();
+
+                Console.WriteLine(
+                    $"Payment ID recebido: {paymentId}"
+                );
+            }
 
             return Ok();
         }
